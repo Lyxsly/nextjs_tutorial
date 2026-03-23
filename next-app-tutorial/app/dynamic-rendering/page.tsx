3d25f6835cbf77204b9ea06b807f3db1295c40c6
Lyxsly <lyxs7y@gmail.com>
2026-03-23 22:48:25 +0900

    基礎勉強

---
+++ next-app-tutorial/app/dynamic-rendering/page.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { Box } from "../components/Box";
+
+const DynamicRenderingPage = () => {
+  return <Box>Dynamic Rendering Page</Box>;
+};
+
+export default DynamicRenderingPage;
